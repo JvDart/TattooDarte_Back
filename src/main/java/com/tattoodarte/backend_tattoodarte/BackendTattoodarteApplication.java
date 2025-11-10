8efea1d2ec0bcb0a4f6129c9d0597bbfb8fb9ca9
jdart <jdart@seidor.es>
2025-11-10 16:32:36 +0100

    --Backend funcionando y dependencias correctas. El mssql-jdbc esta localmente en la carpeta libs

---
+++ src/main/java/com/tattoodarte/backend_tattoodarte/BackendTattoodarteApplication.java
@@ -0,0 +1,13 @@
+package com.tattoodarte.backend_tattoodarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTattoodarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTattoodarteApplication.class, args);
+	}
+
+}
